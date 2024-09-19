6291e7f6a0ae9a02801d170b82d43df30240ab89
Wenjing <u7727187@anu.edu.au>
2024-09-19 16:52:36 +1000

    test

--- app/src/test/java/au/edu/anu/comp6120/thu16_a3_d/CommonHelperTest.java
+++ app/src/test/java/au/edu/anu/comp6120/thu16_a3_d/CommonHelperTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CommonHelperTest {
     @Test
     void testIsValidUUID() {
+
+
         assertTrue(CommonHelper.isValidUUID("87b218d7-2e3e-412d-bb96-dcec1d45d42d"));
         assertFalse(CommonHelper.isValidUUID("-41d4-a716-44665544000"));
     }
