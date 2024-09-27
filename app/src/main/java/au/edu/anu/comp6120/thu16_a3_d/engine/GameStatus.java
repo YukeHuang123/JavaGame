2491986883f84a2890b16f527500da9f3aed802f
Wenjing <u7727187@anu.edu.au>
2024-09-27 23:13:39 +1000

    make the printed line more formal

--- app/src/main/java/au/edu/anu/comp6120/thu16_a3_d/engine/GameStatus.java
+++ app/src/main/java/au/edu/anu/comp6120/thu16_a3_d/engine/GameStatus.java
@@ -11,7 +11,7 @@ public enum GameStatus {
     @Override
     public String toString() {
         switch (this){
-            case READY_MOVE -> {return "Ready Move press W / A /S / D to move";}
+            case READY_MOVE -> {return "Ready Move press W / A / S / D to move";}
             case FIND_BONUS -> {return "Find Bonus press Y or N to take or drop";}
             case MEET_NPC -> {return "Find NPC press Y or N to fight or run away";}
             case FIGHTING -> {return "In Fighting with NPC, choose the weapon (press the index) every time to fight";}
